72c7bec5c8cf9c6dd8cf65ebbc77383bdac23c77
Rashmi R S <rashmirangana.surpu@hcl.com>
2025-12-01 15:05:12 +0530

    Page1 added to Master Branch

--- GitExample6/Controllers/HomeController.cs
+++ GitExample6/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace GitExample6.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
     }
 }
